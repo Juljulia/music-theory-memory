710685b5967c96aa1c6ca250e65f9f97ddc7bba1
Julia Karlsson <karlssonjulia.m@gmail.com>
2019-12-03 19:22:03 +0100

    Added flip cards functions

--- script.js
+++ script.js
@@ -31,11 +31,57 @@ duplicated.forEach(function(card) {
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let stopFlip = false; //lockBoard in tutorial 
 let firstCard, secondCard;
 
 function flipCard() {
+    if(stopFlip) return;
+    if(this === firstCard) return;
+
     /*this represents the card that was clicked*/
     this.classList.add('flip');
+
+    if(!hasFlippedCard){
+        hasFlippedCard= true;
+        firsCard = this;
+        return;
+    }
+
+    secondCard = this;
+
+    checkForMatch();
+}
+
+function checkForMatch(){
+    if(firstCard.dataset.value === secondCard.dataset.value){
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards(){
+    stopFlip = true;
+
+    setTimeout(()=> {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
